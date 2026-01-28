ab4065b1e1343ea5e5aa152a015df4330120a682
OnlineGBC <OnlineGBC@users.noreply.github.com>
2026-01-27 22:44:00 -0500

    Adding backedn files

---
+++ backend/app/database.py
@@ -0,0 +1,15 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+# SQLite file in backend/ (same folder where you run uvicorn)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./local.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
